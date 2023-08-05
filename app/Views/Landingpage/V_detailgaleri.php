93d4fd47267d68519a7655aa2d07ce93a87c57cd
muhisyam <59336299+muhhisyam01@users.noreply.github.com>
2023-08-05 15:09:20 +0800

    Update V_detailgaleri.php
    detail galeri

--- app/Views/Landingpage/V_detailgaleri.php
+++ app/Views/Landingpage/V_detailgaleri.php
@@ -1,4 +1,17 @@
+<!--Page Title / Style Two-->
+<section class="page-title style-two" style="background-image:url(/ikon/images/background/9.jpg)">
+    <div class="auto-container">
+        <h1>Detail geleri</h1>
+        <ul class="page-breadcrumb">
+            <li><a href="User">Home</a></li>
+            <li><a href="User_berita">Gellery</a></li>
+            <li>Detail Gellery</li>
+        </ul>
+    </div>
+</section>
 
+
+    
  <!--Case Single Section-->
  <section class="case-single-section">
     	<div class="auto-container">
@@ -67,11 +80,38 @@
             
             <!--Description Box-->
             <div class="description-box">
-            <h2>Keterangan Foto</h2>
-            <div class="text"><?= $detail['keterangan_galery']; ?></div>
-            <div class="row clearfix">
+            	<h2>Project Description</h2>
+                <div class="text">Must explain to you how all this mistaken idea  denouncing pleasure praising pain was born and I will give you a complete account of the system, expound the actual teachings of the great explorer of the truth, the master-builder of human happiness do not know how to pursue. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it pain, but because occasionally circumstances occur which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise.</div>
+                <div class="row clearfix">
+                	
+                    <!-- <div class="case-info-box col-md-6 col-sm-12 col-xs-12">
+                    	<div class="box-inner">
+                        	<h3>Challenge</h3>
+                            <div class="bold-text">The CEO of Delaware, William Rogerson, made it clear to SEO Inc. that his goals were to:</div>
+                            <ul class="list-style-one">
+                            	<li>Increase organic traffic 5% per month</li>
+                                <li>Increase the number of keywords on the first two pages of Google results.</li>
+                                <li>Increase appointments in both the US and UK</li>
+                                <li>Increase franchisee applications</li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                    <div class="case-info-box col-md-6 col-sm-12 col-xs-12">
+                    	<div class="box-inner">
+                        	<h3>Solution</h3>
+                            <div class="bold-text">Explain to you how all this mistaken idea of denouncing pleasure and praising system.</div>
+                            <ul class="list-style-one">
+                            	<li>Increase organic traffic 5% per month</li>
+                                <li>Increase the number of keywords on the first two pages of Google results.</li>
+                                <li>Increase appointments in both the US and UK</li>
+                                <li>Increase franchisee applications</li>
+                            </ul>
+                        </div>
+                    </div>
+                     -->
+                </div>
             </div>
-        </div>
             <!--End Description Box-->
             
             <!--Case Stats-->
@@ -102,3 +142,4 @@
     <!--End Case Single Section--> 
     
    
+
