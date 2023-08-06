f2fd540494324e702ae219f53a11607c6f79d0b0
muhisyam <59336299+muhhisyam01@users.noreply.github.com>
2023-08-06 23:53:26 +0800

    Update V_contact.php

--- app/Views/Landingpage/V_contact.php
+++ app/Views/Landingpage/V_contact.php
@@ -1,26 +1,9 @@
 <!--Page Title / Style Two-->
-<<<<<<< HEAD
-<section class="contact-banner-post" style="background-image:url(ikon/images/background/9.jpg)">
-    <div class="auto-container">
-        <div class="upper-box">
-            <h2><span class="icon-box flaticon-mail"></span> BNNP <br> Badan Narkotika Nasional Provinsi Selawesi Barat</h2>
-        </div>
 
-        <!--Contact Info Blocks-->
-        <div class="contact-info-blocks">
-            <div class="row clearfix">
-
-                <!--Info Branch Block-->
-                <div class="info-branch-block col-md-4 col-sm-6 col-xs-12">
-                    <div class="inner-box">
-                        <h3>ALAMAT</h3>
-                        <div class="text">Jalan <br> Yos Sudarson Nomor 49 <br> Mamuju</div>
-                        <a href="#" class="direction">Get Direction</a>
-=======
 <section class="contact-banner-post" style="background-image:url(ikon/images/background/1.png)">
     	<div class="auto-container">
         	<div class="upper-box">
-            	<h2><span class="icon-box flaticon-mail"></span> Contact Our Team Today! <br> and Discuss About What You Need.</h2>
+            	<h2><span class="icon-box flaticon-mail"></span> Contact Team kami! <br> dan berikan masukan </h2>
             </div>
             
             <!--Contact Info Blocks-->
@@ -61,21 +44,21 @@
                 <!--Content Column-->
                 <div class="content-column col-md-8 col-sm-12 col-xs-12">
                 	<div class="inner-column">
-                    	<h2>Who do you want to communicate with?</h2>
-                        <div class="text">For more information here we provide seperate department contact person details, pls contact them.</div>
+                    	<h2>Bagi anda yang ingin info lebih lanjut suilahkan hubungi nomor yang tertera dibawah</h2>
+                        <div class="text"></div>
                         <div class="row clearfix">
                         
                         	<!--Communicate Box-->
                             <div class="communicater-box col-md-6 col-sm-6 col-xs-12">
-                            	<h3>Career Inquiries</h3>
+                            	<h3></h3>
                                 <div class="communicate-inner">
                                 	<div class="image">
                                     	<img src="<?php echo base_url('ikon/images/resource/1.png')?>" alt="" />
                                     </div>
-                                    <div class="name">Charles Mecky</div>
+                                    <div class="name">CALL CENTER</div>
                                     <ul>
-                                    	<li>Ph: 936.452.8005</li>
-                                        <li>E: recruiting@ikonteam.com</li>
+                                    	<li>Ph: 0853 4828 0119</li>
+                                        <li>E: BNNP@gmail.com</li>
                                     </ul>
                                 </div>
                             </div>
@@ -83,7 +66,7 @@
                            
                             
                         </div>
->>>>>>> 204085f148ca96d0755d299571b60244abbb49da
+
                     </div>
                 </div>
 
